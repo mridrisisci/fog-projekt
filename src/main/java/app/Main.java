0b9f29428bd01a6ac8d58a5400d5828accec3d67
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-12 13:15:14 +0100

    git repo er oprettet

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,34 @@
+package app;
+
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main
+{
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "carport_db";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7071);
+
+        // Routing
+
+        app.get("/", ctx -> ctx.render("//") );
+    }
+}
