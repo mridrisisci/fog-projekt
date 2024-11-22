30f8e1e0a2a62c3ee2ddff35772a2e42c2f12335
Crispy212 <Christofferleisted@gmail.com>
2024-11-22 14:26:10 +0100

    Added OrderMapper.java til persistence

---
+++ src/main/java/app/persistence/OrderMapper.java
@@ -0,0 +1,10 @@
+package app.persistence;
+
+public class OrderMapper
+{
+    public Order getOrder(){}
+    public List<Order>getOrders(){}
+    public void deleteOrderByID(){}
+    public void updateOrderByUserID(){}
+    public void addOrderToDB(){}
+}
