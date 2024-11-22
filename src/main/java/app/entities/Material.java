88e01cda5c31b2ca6140faf48206064223361771
Crispy212 <Christofferleisted@gmail.com>
2024-11-22 13:58:55 +0100

    Added Material.java til entities

---
+++ src/main/java/app/entities/Material.java
@@ -0,0 +1,16 @@
+package app.entities;
+
+public class Material
+{
+    private int materialID;
+    private String materialName;
+    private String description;
+    private int purchasePrice;
+    private String unit;
+    private int quantity;
+    private int length;
+    private int height;
+    private int width;
+    private boolean hasShedAddons;
+    public int getNumbersOfShedAddons;
+}
