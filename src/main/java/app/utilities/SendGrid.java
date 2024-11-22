8efaaae0fcb3960922abf5c84ff88f6aac9e945c
Crispy212 <Christofferleisted@gmail.com>
2024-11-22 14:26:28 +0100

    Added SendGrid.java til utilities

---
+++ src/main/java/app/utilities/SendGrid.java
@@ -0,0 +1,12 @@
+package app.utilities;
+
+public class SendGrid
+{
+    private String API_KEY;
+    private String salespersonEmail;
+    private String customerEmail;
+    private String adminEmail;
+    private String subjectLine;
+    private String body;
+    public SendGrid(String to,String name,String from){}
+}
