559bfe91346ac264fb7a3800d7f6296f4f5c4878
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-22 15:16:51 +0100

    fejl i accountmapper er rettet

--- src/main/java/app/persistence/AccountMapper.java
+++ src/main/java/app/persistence/AccountMapper.java
@@ -1,11 +1,33 @@
 package app.persistence;
 
+import app.entities.Account;
+
+import java.util.List;
+
 public class AccountMapper
 {
-    public void addAccount(){}
-    public void deleteAccount(Account account){}
-    public void updateAccount(Account account){}
-    public Account getAccountByID(Account account){}
-    public List<Account>getAllAccounts(Account account){}
-    public void login(){}
+    public static void addAccount()
+    {
+
+    }
+    public static void deleteAccount(Account account)
+    {
+
+    }
+    public static void updateAccount(Account account)
+    {
+
+    }
+    public static Account getAccountByID(Account account)
+    {
+        return null;
+    }
+    public static List<Account> getAllAccounts(Account account)
+    {
+        return null;
+    }
+    public static void login()
+    {
+
+    }
 }
