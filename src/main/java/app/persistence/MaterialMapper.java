f6a6f9bd3bee662df145ce5c195b3aef14ca2e6b
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-22 14:11:42 +0100

    Lavet klassen og skrevet navnet på de metoder der står i klassediagrammet - ændret et navn fra product til material

---
+++ src/main/java/app/persistence/MaterialMapper.java
@@ -0,0 +1,32 @@
+package app.persistence;
+
+public class MaterialMapper
+{
+    public Material getMaterial()
+    {
+    }
+
+    public Material getMaterialByID()
+    {
+    }
+
+    public void updateMaterial()
+    {
+    }
+
+    public void updateMaterialByPrice()
+    {
+    }
+
+    public void addMaterialToDB()
+    {
+    }
+
+    public void deleteMaterial()
+    {
+    }
+
+    public List<Material> getAllMaterials()
+    {
+    }
+}
