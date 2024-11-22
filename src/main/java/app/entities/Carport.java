465b7ef2ab45cf3a8584549a8b99015a5e428330
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-22 13:57:32 +0100

    Tilføjet attributer og metodenavne

--- src/main/java/app/entities/Carport.java
+++ src/main/java/app/entities/Carport.java
@@ -1,5 +1,59 @@
 package app.entities;
 
+import java.util.List;
+
 public class Carport
 {
+    private final int HEIGHT;
+    private final int WIDTH;
+    private final int LENGTH;
+    private boolean hasShed;
+    private RoofType roofType;
+    public List<Material> materialsList;
+
+    public Carport()
+    {
+
+    }
+
+    public int calcCarportMaterial()
+    {
+        return 0;
+    }
+
+    public void calcCarportPrice()
+    {
+
+    }
+
+    public Material calcPosts(Material material)
+    {
+        return null;
+    }
+
+    public Material calcBeams(Material material)
+    {
+        return null;
+    }
+
+    public Material calcRafters(Material material)
+    {
+        return null;
+    }
+
+    public int getRafters()
+    {
+        return 0;
+    }
+
+    public int getBeams()
+    {
+        return 0;
+    }
+
+    public int getPosts()
+    {
+        return 0;
+    }
+
 }
