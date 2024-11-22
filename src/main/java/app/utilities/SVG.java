9dfe9799ba1f8e79e6c1ab8808429a3dca170738
Crispy212 <Christofferleisted@gmail.com>
2024-11-22 14:26:37 +0100

    Added SVG.java til utilities

---
+++ src/main/java/app/utilities/SVG.java
@@ -0,0 +1,14 @@
+package app.utilities;
+
+public class SVG
+{
+    private StringBuilder svg;
+    public SVG(){}
+    public addRectangle(){}
+    public addLine(){}
+    public addArrow(){}
+    public addText(){}
+    public addSvg(){}
+    @Override
+    public toString(){}
+}
