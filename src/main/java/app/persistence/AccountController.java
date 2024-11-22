34c334cf30f2890600b6b749695a7996e7447fa2
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-22 14:02:12 +0100

    Lavet klasse der hedder AccountController

---
+++ src/main/java/app/persistence/AccountController.java
@@ -0,0 +1,17 @@
+package app.persistence;
+
+public class AccountController
+{
+    public void addRoutes()
+    {
+    }
+
+    private User login(){
+        return null;
+    }
+
+    private void logout()
+    {
+
+    }
+}
