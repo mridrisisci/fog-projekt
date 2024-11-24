3102193c0dc2d31ccf5b46d9a5c39e9050296f35
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-24 13:04:00 +0100

    oprettede validerings-metoder i controlleren til formularen. (email ikke færdig endnu)

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -25,46 +25,91 @@ public class OrderController
 
     private static void createQuery(Context ctx, ConnectionPool dbConnection)
     {
-        String carportWidth = ctx.formParam("carportwidth");
-        String carportHeight = ctx.formParam("carportheight");
-        String trapeztag = ctx.formParam("trapeztag");
-        String shedWidth = ctx.formParam("shedwidth");
-        String shedLength = ctx.formParam("shedlength");
-        String specialRequests = ctx.formParam("specialrequests");
+        String carportWidth = ctx.formParam("chooseWidth");
+        String carportHeight = ctx.formParam("chooseHeight");
+        String trapeztag = ctx.formParam("chooseRoof");
+        String shedWidth = ctx.formParam("chooseShedWidth");
+        String shedLength = ctx.formParam("chooseShedLength");
+        String specialRequests = ctx.formParam("specialWishes");
 
-        String name = ctx.formParam("name");
-        String address = ctx.formParam("address");
-        String postalCode = ctx.formParam("postalcode");
-        String city = ctx.formParam("city");
-        String phone = ctx.formParam("phone");
-        String email = ctx.formParam("email");
-        String consent = ctx.formParam("consent");
+        String name = ctx.formParam("customername");
+        String address = ctx.formParam("chooseAddress");
+        String postalCode = ctx.formParam("choosePostalCode");
+        String city = ctx.formParam("chooseCity");
+        String phone = ctx.formParam("choosePhoneNumber");
+        String email = ctx.formParam("chooseEmail");
+        String consent = ctx.formParam("chooseConsent");
+
+
+        // TODO: check at form-parameetrene ikke er null
+        // TODO: validere kundens telefon nummer
+        validatePhoneNumber(ctx, "choosePhoneNumber");
+        // TODO: validere kundens email
+        validateEmail(ctx, "chooseEmail");
+        // TODO: validere kundens postnummer
+        validatePostalCode(ctx, "choosePostalCode");
+        // TODO: Oprette kundens ordre i 'orders'tabellen
+
+
         List<Order> carportOrder = new ArrayList<>();
+
         List<String> params = new ArrayList<>(Arrays.asList(
             carportWidth, carportHeight, trapeztag, shedWidth, shedLength, specialRequests,
             name, address, postalCode, city, phone, email, consent
         ));
 
-/*
-        try
-        {
 
-
-            if (params.size() == 12)
-            {
-
-                OrderMapper.createQueryInOrders(int customerID, int carportID, int salesPersonID, String status, Timestamp timeStamp,
-                                                int carportHeight, int carportWidth, boolean hasShed, String roofType, int accountID, ConnectionPool pool);
-            }
-
-        } catch (SQLException e)
-        {
-            ctx.attribute("message", e.getMessage());
-        }*/
 
 
     }
 
+    private static boolean validatePostalCode(Context ctx, String postalCode)
+    {
+        int p = Integer.parseInt(postalCode); // does it need to be parsed ?
+
+        if (postalCode.length() != 4 || postalCode.length() < 4 || postalCode.length() > 4)
+        {
+            return false;
+        }
+        if (postalCode.length() == 4)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean validateEmail(Context ctx, String email)
+    {
+        String chars = "@.";
+        boolean hasChar = email.chars().anyMatch(ch -> chars.indexOf(ch) >= 0);
+        if (!email.equals(hasChar))
+        {
+            return false;
+        }
+        return false;
+
+    }
+
+    private static void validatePostalCode()
+    {
+
+    }
+
+    private static boolean validatePhoneNumber(Context ctx, String number)
+    {
+        String numbers = "1234567890";
+        boolean hasNumber = number.chars().anyMatch(ch -> numbers.indexOf(ch) >= 0);
+
+        if (number.length() < 8)
+        {
+            ctx.attribute("message", "Dit telefonnummer er ugyldigt");
+            return false;
+        } else if (number.length() == 8 && hasNumber)
+        {
+            return true;
+        }
+        return false;
+    }
     private static boolean validateParams(String... params)
     {
         for (String p : params)
