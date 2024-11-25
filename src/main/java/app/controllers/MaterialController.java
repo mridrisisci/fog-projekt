c024da3b206416ce9130ac07832a2adfcea37e16
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-25 21:59:44 +0100

    der lavet et forslag / udkast til beregning af Stolper i materialcontroller + mapper-metode til at lave en materialList + tilføjet 2 linier til controller-metoden i Order

--- src/main/java/app/controllers/MaterialController.java
+++ src/main/java/app/controllers/MaterialController.java
@@ -1,7 +1,12 @@
 package app.controllers;
 
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.MaterialMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.sql.SQLException;
 
 public class MaterialController
 {
@@ -9,4 +14,33 @@ public class MaterialController
     public static void addRoutes(Javalin app, ConnectionPool dBConnection)
     {
     }
+
+
+    // Eks. på CalcPosts()
+    public static int[] calcPosts(int height, int width, Context ctx, ConnectionPool pool)
+    {
+        int[] quantity = new int[2];
+
+        quantity[0] = height;
+        quantity[1] = width;
+
+        // TODO: Der skal nok testes lidt på tallene så vi sørger for mindst mulig spild af materiale
+        // TODO: Evt. laves en unit test her lidt ala. det Andrës gruppe lavede i går ( calcOptimalWood() )
+        final int POSTS = 2 * (2 / height + width) + 10; // 10 er evt. det forbehold vi tager for mængden af spild?
+
+        try
+        {
+            MaterialMapper.createMaterialList(POSTS, pool);
+
+        } catch (DatabaseException e)
+        {
+            System.out.println(e.getMessage());
+            ctx.attribute("message", e.getMessage());
+        }
+        return quantity;
+
+
+        //return POSTS; // antal posts, der skal hentes vha. mapperen
+    }
+
 }
