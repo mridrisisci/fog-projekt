58a5e3ad1e25e22aa08aa640ce3c959a8e95c39b
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-25 15:12:52 +0100

    Begyndelse på udregninger/kommentar dertil

--- src/main/java/app/entities/Carport.java
+++ src/main/java/app/entities/Carport.java
@@ -1,5 +1,8 @@
 package app.entities;
 
+import app.persistence.MaterialMapper;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Carport
@@ -9,7 +12,7 @@ public class Carport
     private final int LENGTH;
     private boolean hasShed;
     private RoofType Rooftype;
-    public List<Material> materialList;
+    private List<Material> materialList = new ArrayList<>();
 
     public Carport(List<Material> materialList, RoofType rooftype, boolean hasShed, int LENGTH, int WIDTH, int HEIGHT)
     {
@@ -35,29 +38,111 @@ public class Carport
      * så skal der være en metode der beregner salgspris
      * en der beregner dækningspris*/
 
-    public Material calcPosts(Material material)
+    //Stople
+    public int calcPosts(Order order)
     {
-        if (WIDTH <= 300 && LENGTH <= 300)
+        //Antal stolper
+        int quantity;
+        //150 er en buffer fra starten og slutningen af remmen - 9,7 er bredden på stolpen, og til at starte med har man minimum to stolper per længde
+        if (getLENGTH()-150-9.7-9.7 <= 480)
         {
-            int posts = 3;
-        } else if (WIDTH <= 300 && LENGTH > 300)
+            quantity = 4;
+        } else {
+            quantity = 6;
+        }
+
+        //Længde på stolperne - sikre vi får fat i den korrekte variant
+        List<Material> materialList1 = (List<Material>) MaterialMapper.getMaterialByID();
+            //Linjen er castet
+        Material material1 = materialList1.get(0);
+
+        return quantity;
+
+    }
+
+    //Remme
+    public Material calcBeams(Material material)
+    {
+    //Der skal være minimum 2 af alt, da der på hver side af carporten er en rem.
+        //Mangler at kalde materiale der skal bruges
+        int quantity = 2;
+        if(getLENGTH() <= 270){
+            material = material.getMaterialID();
+            //Woodmaterial length = 300
+            quantity = 2;
+        } else if (getLENGTH() <= 330)
         {
-         int posts = 5;
+            material.getLength();
+            //Woodmaterial length = 360
+            quantity = 2;
+        } else if (getLENGTH() <= 390)
+        {
+            material.getLength();
+            //Woodmaterial length = 420
+            quantity = 2;
+        } else if (getLENGTH() <= 450)
+        {
+            material.getLength();
+            //Woodmaterial length = 480
+            quantity = 2;
+        } else if (getLENGTH() <= 510)
+        {
+            material.getLength();
+            //Woodmaterial length = 540
+            quantity = 2;
+        } else if (getLENGTH() <= 600)
+        {
+            material.getLength();
+            //Woodmaterial length = 600
+            quantity = 2;
+        } else if (getLENGTH() <= 690) {
+            material.getLength();
+            //Woodmaterial length = 360
+            quantity = 4;
+        } else if (getLENGTH() <= 780){
+            material.getLength();
+            //Woodmaterial length = 420
+            quantity = 4;
+
         }
 
         return material;
 
     }
 
-    public Material calcBeams(Material material)
+    //Understern
+    public Material calcBottomFasciaBoard(Material material)
     {
-        return material;
+        if (getWIDTH() < 300)
+        {
 
+        }
+
+        return material;
     }
 
-    public Material calcRafters(Material material)
+
+    //Overstern
+    public Material calcTopFasciaBoard(Material material)
     {
         return material;
+    }
+
+
+    //Lægter
+    public Material calcBattern(Material material)
+    {
+        return material;
+    }
+
+    //Spær
+    public Material calcRafters(Material material)
+    {
+        Material newMaterial = material;
+
+        //den går igennem restSpær gennem, hvis der er 55, sæt et spær og minus 45
+
+        return newMaterial;
     }
 
     public int getHEIGHT()
