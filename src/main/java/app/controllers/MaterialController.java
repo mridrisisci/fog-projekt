cbfb11417d7fbde62fbafcbace79abc96b89be7e
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-26 16:03:15 +0100

    udkast til beregning af x-posts

--- src/main/java/app/controllers/MaterialController.java
+++ src/main/java/app/controllers/MaterialController.java
@@ -1,7 +1,14 @@
 package app.controllers;
 
+import app.entities.Carport;
+import app.entities.Material;
+import app.exceptions.DatabaseException;
 import app.persistence.ConnectionPool;
+import app.persistence.MaterialMapper;
 import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.sql.SQLException;
 
 public class MaterialController
 {
@@ -9,4 +16,37 @@ public class MaterialController
     public static void addRoutes(Javalin app, ConnectionPool dBConnection)
     {
     }
+
+
+    // Eks. på CalcPosts()
+    public static void calcPosts(int height, int width, Context ctx, ConnectionPool pool)
+    {
+        int[] quantity = new int[2];
+
+        quantity[0] = height;
+        quantity[1] = width;
+
+        // TODO: Der skal nok testes lidt på tallene så vi sørger for mindst mulig spild af materiale
+        // TODO: Evt. laves en unit test her lidt ala. det Andrës gruppe lavede i går ( calcOptimalWood() )
+        final int POSTS = 4;
+        Material type  = new Material("post");
+
+        try
+        {
+            MaterialMapper.createMaterialList(POSTS, type, pool);
+
+        } catch (DatabaseException e)
+        {
+            System.out.println(e.getMessage());
+            ctx.attribute("message", e.getMessage());
+        }
+
+
+        //return POSTS; // antal posts, der skal hentes vha. mapperen
+    }
+
+
+
+
+
 }
--- src/main/java/app/persistence/MaterialMapper.java
+++ src/main/java/app/persistence/MaterialMapper.java
@@ -1,11 +1,63 @@
 package app.persistence;
 
 import app.entities.Material;
+import app.exceptions.DatabaseException;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class MaterialMapper
 {
+
+    public static List<Material> createMaterialList(int materialQuantity, Material material, ConnectionPool pool) throws DatabaseException
+    {
+        // TODO: Muligvis en join her? eller alternativt en SQL der henter fra forbindelsestabellen?
+        String sql = "SELECT name, unit, description, type FROM materials";
+
+        String name;
+        String unit;
+        String description;
+        String type = material.getType();
+
+
+
+
+        try (Connection connection = pool.getConnection();
+        PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ResultSet rs = ps.executeQuery();
+            List<Material> materialList = new ArrayList<>();
+            while(rs.next())
+            {
+                // TODO: Mangler en if-statement, der checker om material er == post
+                // TODO: brug 'materialQuantity' (antal posts) til at bestemme antallet af materiale der hentes fra db
+                // hvordan checker vi typen af et materiale?
+                name = rs.getString("name");
+                unit = rs.getString("unit");
+                description = rs.getString("description");
+                String materialType = rs.getString("type");
+                int quantity = materialQuantity;
+                if (materialType == type)
+                {
+                    while (quantity != materialQuantity)
+                    {
+                        materialList.add(new Material(name, unit, type, description, quantity));
+                    }
+
+                }
+            }
+            return materialList;
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            throw new DatabaseException(e.getMessage());
+        }
+
+    }
+
     public static Material getMaterial()
     {
         return null;
