84bc9f54ae45407dc59d6898aeb51a8f0cce4297
Gervig <gervig91@gmail.com>
2024-11-27 15:44:39 +0100

    fjernede customerID fra querry metode, og lavede getCarportByOrderID metode

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -1,29 +1,31 @@
 package app.persistence;
+
+import app.entities.Carport;
 import app.entities.Order;
 import app.entities.OrderStatus;
+import app.entities.RoofType;
 import app.exceptions.DatabaseException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.List;
 
 public class OrderMapper
 {
 
-    public static void createQueryInOrders(int customerID, int carportID, int salesPersonID, String status, Timestamp orderPlaced,
-                                           int height, int width, boolean hasShed, String roofType, int accountID, ConnectionPool pool) throws DatabaseException
+    public static void createQueryInOrders(int accountID, int carportID, int salesPersonID, String status, Timestamp orderPlaced,
+                                           int height, int width, boolean hasShed, String roofType, ConnectionPool pool) throws DatabaseException
     {
-        String sql = "INSERT INTO orders (customer_id, carport_id, salesperson_id, status, " +
-            "order_placed, height, width, has_shed, roof_type, account_id) " +
-            "VALUES (?,?,?,?,?,?,?,?,?,?);";
+
+        //TODO: fix hasShed
+        String sql = "INSERT INTO orders (account_id, carport_id, salesperson_id, status, " +
+                "order_placed, height, width, hasShed, roof_type, account_id) " +
+                "VALUES (?,?,?,?,?,?,?,?,?,?);";
 
 
         try (Connection connection = pool.getConnection();
-            PreparedStatement ps = connection.prepareStatement(sql))
+             PreparedStatement ps = connection.prepareStatement(sql))
         {
-            ps.setInt(1, customerID);
+            ps.setInt(1, accountID);
             ps.setInt(2, carportID);
             ps.setInt(3, salesPersonID);
             ps.setString(4, status);
@@ -32,7 +34,6 @@ public class OrderMapper
             ps.setInt(7, width);
             ps.setBoolean(8, hasShed);
             ps.setString(9, roofType);
-            ps.setInt(10, accountID);
 
             int rowsAffected = ps.executeUpdate();
             if (rowsAffected != 1)
@@ -46,27 +47,68 @@ public class OrderMapper
         }
 
 
-
     }
 
     public static Order getOrder()
     {
         return null;
     }
+
     public static List<Order> getOrders()
     {
         return null;
     }
+
     public static void deleteOrderByID()
     {
 
     }
+
     public static void updateOrderByUserID()
     {
 
     }
+
     public static void addOrderToDB()
     {
 
+    }
+
+    //TODO lav metode med return type Carport,
+    // der henter data fra orders table i DB for et given orderID
+    public static Carport getCarportByOrderID(int orderID, ConnectionPool connectionPool) throws DatabaseException
+    {
+        Carport carport = null;
+        String sql = "";
+
+        try (Connection connection = connectionPool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, orderID);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+            {
+                int width = rs.getInt("width");
+                int length = rs.getInt("length");
+                boolean hasShed = rs.getBoolean("hasShed");
+                //TODO: lav String om til RoofType objekt
+//                RoofType roofType = rs.getString("roof_type");
+                carport = new Carport(roofType, hasShed, length, width);
+            }
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected != 1)
+            {
+                throw new DatabaseException("fejl");
+            }
+        } catch (SQLException | DatabaseException e)
+        {
+            System.out.println(e.getMessage());
+            throw new DatabaseException(e.getMessage());
+        }
+
+        return carport;
     }
 }
