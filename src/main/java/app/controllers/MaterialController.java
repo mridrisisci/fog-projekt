653a3ba888f0344ff954d6c643a7868ebdd9972a
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-27 10:10:45 +0100

    Ændret calcPosts metode så den tager oplysninger fra carport.calcPosts

--- src/main/java/app/controllers/MaterialController.java
+++ src/main/java/app/controllers/MaterialController.java
@@ -12,22 +12,19 @@ import java.sql.SQLException;
 public class MaterialController
 {
 
+
     public static void addRoutes(Javalin app, ConnectionPool dBConnection)
     {
     }
 
 
     // Eks. på CalcPosts()
-    public static int[] calcPosts(int height, int width, Context ctx, ConnectionPool pool)
+    public static int calcPosts(Carport carport, Context ctx, ConnectionPool pool)
     {
-        int[] quantity = new int[2];
-
-        quantity[0] = height;
-        quantity[1] = width;
 
         // TODO: Der skal nok testes lidt på tallene så vi sørger for mindst mulig spild af materiale
         // TODO: Evt. laves en unit test her lidt ala. det Andrës gruppe lavede i går ( calcOptimalWood() )
-        final int POSTS = 1; //Skal hentes fra Carport calcPosts
+        final int POSTS = carport.calcPosts(); //Skal hentes fra Carport calcPosts
 
         try
         {
@@ -38,12 +35,11 @@ public class MaterialController
             System.out.println(e.getMessage());
             ctx.attribute("message", e.getMessage());
         }
-        return quantity;
+        return POSTS;
 
 
         //return POSTS; // antal posts, der skal hentes vha. mapperen
     }
-
 
 
 }
