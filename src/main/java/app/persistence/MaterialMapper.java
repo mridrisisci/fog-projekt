367d1376d8930077a8907878574b6838a9793b63
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-27 10:50:11 +0100

    Lavet fasciaboard metoder

--- src/main/java/app/persistence/MaterialMapper.java
+++ src/main/java/app/persistence/MaterialMapper.java
@@ -23,13 +23,12 @@ public class MaterialMapper
         String description;
 
 
-
         try (Connection connection = pool.getConnection();
-        PreparedStatement ps = connection.prepareStatement(sql))
+             PreparedStatement ps = connection.prepareStatement(sql))
         {
             ResultSet rs = ps.executeQuery();
             List<Material> materialList = new ArrayList<>();
-            while(rs.next())
+            while (rs.next())
             {
                 // TODO: Mangler en if-statement, der checker om material er == post
                 // TODO: brug 'materialQuantity' (antal posts) til at bestemme antallet af materiale der hentes fra db
@@ -48,68 +47,43 @@ public class MaterialMapper
 
     }
 
+    //TODO: Lave pickList som kalder på alle de metoder der udregner materiale, længder og antal
     public static List<Material> createPickList(ConnectionPool pool) throws DatabaseException
     {
-        // TODO: Muligvis en join her? eller alternativt en SQL der henter fra forbindelsestabellen?
-        String sql = "SELECT name, unit, description FROM materials";
-
-        String name;
-        String unit;
-        String description;
-
-
-
-        try (Connection connection = pool.getConnection();
-             PreparedStatement ps = connection.prepareStatement(sql))
-        {
-            ResultSet rs = ps.executeQuery();
-            List<Material> materialList = new ArrayList<>();
-            while(rs.next())
-            {
-                // TODO: Mangler en if-statement, der checker om material er == post
-                // TODO: brug 'materialQuantity' (antal posts) til at bestemme antallet af materiale der hentes fra db
-                // hvordan checker vi typen af et materiale?
-                name = rs.getString("name");
-                unit = rs.getString("unit");
-                description = rs.getString("description");
-                materialList.add(new Material(name, unit, description));
-            }
-            return materialList;
-        } catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-            throw new DatabaseException(e.getMessage());
-        }
-
+        List<Material> pickList = new ArrayList<>();
+        return pickList;
     }
 
+    //TODO: MANGLER TEST!
     public static Material calcSideUnderfasciaBoard(Carport carport, ConnectionPool pool) throws DatabaseException
     {
-        // TODO: Muligvis en join her? eller alternativt en SQL der henter fra forbindelsestabellen?
-        String sql = "SELECT name, unit, description FROM materials WHERE material_id = 1,2 ";
+        String sql = "SELECT material_id, name, unit, description, length FROM public.materials WHERE material_id IN (1,2) AND length = ?;";
 
         String name;
         String unit;
         String description;
-
+        int quantity = carport.calcSidesFasciaBoard()[0];
+        int length = carport.calcSidesFasciaBoard()[1];
+        int materialID;
+        Material underFasciaBoard = null;
 
 
         try (Connection connection = pool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql))
         {
+            ps.setInt(1, length);
             ResultSet rs = ps.executeQuery();
-            List<Material> materialList = new ArrayList<>();
-            while(rs.next())
+            while (rs.next())
             {
-                // TODO: Mangler en if-statement, der checker om material er == post
-                // TODO: brug 'materialQuantity' (antal posts) til at bestemme antallet af materiale der hentes fra db
                 // hvordan checker vi typen af et materiale?
                 name = rs.getString("name");
                 unit = rs.getString("unit");
                 description = rs.getString("description");
-                materialList.add(new Material(name, unit, description));
+                length = rs.getInt("length");
+                materialID = rs.getInt("material_id");
+                underFasciaBoard = new Material(materialID, name, description, unit, quantity, length);
             }
-            return materialList;
+            return underFasciaBoard;
         } catch (SQLException e)
         {
             System.out.println(e.getMessage());
@@ -118,33 +92,36 @@ public class MaterialMapper
 
     }
 
+    //TODO: MANGLER TEST!
     public static Material calcSideOverfasciaBoard(Carport carport, ConnectionPool pool) throws DatabaseException
     {
-        // TODO: Muligvis en join her? eller alternativt en SQL der henter fra forbindelsestabellen?
-        String sql = "SELECT name, unit, description FROM materials WHERE material_id is 1,2";
+        String sql = "SELECT material_id, name, unit, description, length FROM public.materials WHERE material_id IN (3,4) AND length = ?;";
 
         String name;
         String unit;
         String description;
-
+        int quantity = carport.calcSidesFasciaBoard()[0];
+        int length = carport.calcSidesFasciaBoard()[1];
+        int materialID;
+        Material underFasciaBoard = null;
 
 
         try (Connection connection = pool.getConnection();
              PreparedStatement ps = connection.prepareStatement(sql))
         {
+            ps.setInt(1, length);
             ResultSet rs = ps.executeQuery();
-            List<Material> materialList = new ArrayList<>();
-            while(rs.next())
+            while (rs.next())
             {
-                // TODO: Mangler en if-statement, der checker om material er == post
-                // TODO: brug 'materialQuantity' (antal posts) til at bestemme antallet af materiale der hentes fra db
                 // hvordan checker vi typen af et materiale?
                 name = rs.getString("name");
                 unit = rs.getString("unit");
                 description = rs.getString("description");
-                materialList.add(new Material(name, unit, description));
+                length = rs.getInt("length");
+                materialID = rs.getInt("material_id");
+                underFasciaBoard = new Material(materialID, name, description, unit, quantity, length);
             }
-            return materialList;
+            return underFasciaBoard;
         } catch (SQLException e)
         {
             System.out.println(e.getMessage());
@@ -153,6 +130,81 @@ public class MaterialMapper
 
     }
 
+    //TODO: MANGLER TEST!
+    public static Material calcFrontAndBackUnderfasciaBoard(Carport carport, ConnectionPool pool) throws DatabaseException
+    {
+        String sql = "SELECT material_id, name, unit, description, length FROM public.materials WHERE material_id IN (1,2) AND length = ?;";
+
+        String name;
+        String unit;
+        String description;
+        int quantity = carport.calcFrontAndBackFasciaBoard()[0];
+        int length = carport.calcFrontAndBackFasciaBoard()[1];
+        int materialID;
+        Material underFasciaBoard = null;
+
+
+        try (Connection connection = pool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, length);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                // hvordan checker vi typen af et materiale?
+                name = rs.getString("name");
+                unit = rs.getString("unit");
+                description = rs.getString("description");
+                length = rs.getInt("length");
+                materialID = rs.getInt("material_id");
+                underFasciaBoard = new Material(materialID, name, description, unit, quantity, length);
+            }
+            return underFasciaBoard;
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            throw new DatabaseException(e.getMessage());
+        }
+
+    }
+
+    //TODO: MANGLER TEST!
+    public static Material calcFrontAndBackOverfasciaBoard(Carport carport, ConnectionPool pool) throws DatabaseException
+    {
+        String sql = "SELECT material_id, name, unit, description, length FROM public.materials WHERE material_id IN (3,4) AND length = ?;";
+
+        String name;
+        String unit;
+        String description;
+        int quantity = carport.calcFrontAndBackFasciaBoard()[0];
+        int length = carport.calcFrontAndBackFasciaBoard()[1];
+        int materialID;
+        Material underFasciaBoard = null;
+
+
+        try (Connection connection = pool.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql))
+        {
+            ps.setInt(1, length);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                // hvordan checker vi typen af et materiale?
+                name = rs.getString("name");
+                unit = rs.getString("unit");
+                description = rs.getString("description");
+                length = rs.getInt("length");
+                materialID = rs.getInt("material_id");
+                underFasciaBoard = new Material(materialID, name, description, unit, quantity, length);
+            }
+            return underFasciaBoard;
+        } catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+            throw new DatabaseException(e.getMessage());
+        }
+
+    }
 
 
     public static Material getMaterial()
