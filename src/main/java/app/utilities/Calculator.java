ac27ce5dab73b812676bd40e2cc740f60781596a
RikkeMariussen <cph-rm225@cphbusiness.dk>
2024-11-27 13:29:53 +0100

    Lavet klasse og fjernet calc klasser fra carport-klassen

---
+++ src/main/java/app/utilities/Calculator.java
@@ -0,0 +1,178 @@
+package app.utilities;
+
+import app.entities.Carport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator
+{
+
+    //TODO: Der skal tilføjes mere beregning, hvis der skal tilføjes skur
+    //Stolpe
+    public int calcPosts(Carport carport)
+    {
+        //Antal stolper
+        int quantity;
+        //Længder er omregnet til mm i stedet for cm, ved at gange med 100
+        // 9,7 cm er bredden på stolpen, og til at starte med har man minimum to stolper per længde
+        int overhangDefault = 1000; //overhangDefault er en buffer for start og slut, da man ikke placerer stolper for enden af carport
+        int widthOfPost = 97; //widthOfPost skal modregnes i hvor langt der er mellem eventuelt er stolper
+        int maxSpan = 3100; //maxSpan er spændet der maks. må være melle stolper jf. materialelisten givet
+        int totalWidthWithinMaxSpan = 2*overhangDefault + 2*widthOfPost + maxSpan;
+
+
+        //getLength()*100 for at få mm i stedet for cm, så det kan omregnes i int.
+        if ((carport.getLENGTH()*100) - (2*overhangDefault) - (2*widthOfPost) <= totalWidthWithinMaxSpan)
+        {
+            quantity = 4;
+        } else
+        {
+            quantity = 6;
+        }
+        return quantity;
+    }
+
+    //TODO: Der skal tilføjes mere beregning, hvis der skal tilføjes skur
+    //Remme
+    public int[] calcBeams(Carport carport)
+    {
+        int[] beams = new int[2];
+        int quantity = 0;
+        int length = 0;
+
+        //TODO: Tallene i if-statement skal ændres så det ikke er hardcoded, men hentes fra køberens valgmuligheder
+        if (carport.getLENGTH() <= 300)
+        {
+            //Woodmaterial length = 600
+            length = 600;
+            quantity = 1;
+
+        } else if (300 < carport.getLENGTH() || carport.getLENGTH() <= 480)
+        {
+            //Woodmaterial length = 480
+            length = 480;
+            quantity = 2;
+        } else if (480 < carport.getLENGTH() || carport.getLENGTH() <= 600)
+        {
+            //Woodmaterial length = 600
+            length = 600;
+            quantity = 2;
+        } else if (600 < carport.getLENGTH() || carport.getLENGTH() <= 780)
+        {
+            //Woodmaterial length = 480
+            length = 480;
+            quantity = 4;
+        }
+
+        beams[0] = quantity;
+        beams[1] = length;
+
+        return beams;
+    }
+
+    //TODO: Der skal tilføjes mere beregning, hvis der skal tilføjes skur
+    //Stern på remme
+    public int[] calcSidesFasciaBoard(Carport carport)
+    {
+
+        int[] fascia = new int[2];
+        int quantity = 0;
+        int length = 0;
+
+        //Antallet af brædder og hvilke bræddelængder der skal bruges, er afhængigt af carportens længde
+        //TODO: Tallene i if-statement skal ændres så det ikke er hardcoded, men hentes fra køberens valgmuligheder
+        if (carport.getLENGTH() <= 270)
+        {
+            //Woodmaterial length = 540
+            length = 540;
+            quantity = 1;
+        } else if (270 < carport.getLENGTH() || carport.getLENGTH() <= 360)
+        {
+            //Woodmaterial length = 360
+            length = 360;
+            quantity = 2;
+        } else if (360 < carport.getLENGTH() || carport.getLENGTH() <= 540)
+        {
+            //Woodmaterial length = 540
+            length = 540;
+            quantity = 2;
+        } else if (540 < carport.getLENGTH() || carport.getLENGTH() <= 690)
+        {
+            //Woodmaterial length = 360
+            length = 360;
+            quantity = 4;
+        } else if (690 < carport.getLENGTH() || carport.getLENGTH() <= 780)
+        {
+            //Woodmaterial length = 540
+            length = 540;
+            quantity = 4;
+        }
+
+        fascia[0] = quantity;
+        fascia[1] = length;
+
+        return fascia;
+    }
+
+    //TODO: Der skal tilføjes mere beregning, hvis der skal tilføjes skur
+    //Stern til for- og bagside
+    public int[] calcFrontAndBackFasciaBoard(Carport carport)
+    {
+        int[] fascia = new int[2];
+        int quantity = 0;
+        int length = 0;
+
+        //Antallet af brædder og hvilke bræddelængder der skal bruges, er afhængigt af carportens bredde
+        //TODO: Tallene i if-statement skal ændres så det ikke er hardcoded, men hentes fra køberens valgmuligheder
+        if (carport.getWIDTH() <= 270)
+        {
+            //Woodmaterial length = 540
+            length = 540;
+            quantity = 1;
+        } else if (270 < carport.getWIDTH() || carport.getWIDTH() <= 360)
+        {
+            //Woodmaterial length = 360
+            length = 360;
+            quantity = 2;
+        } else if (360 < carport.getWIDTH() || carport.getWIDTH() <= 540)
+        {
+            //Woodmaterial length = 540
+            length = 540;
+            quantity = 2;
+        } else if (540 < carport.getWIDTH() || carport.getWIDTH() <= 600)
+        {
+            //Woodmaterial length = 360
+            length = 360;
+            quantity = 4;
+        }
+
+        fascia[0] = quantity;
+        fascia[1] = length;
+
+        return fascia;
+    }
+
+    //Spær
+    public int calcRafters(Carport carport)
+    {
+        int lengthMM = carport.getLENGTH();
+        //Der er 60 cm mellem hvert spær, og ca. 55 cm fra inderside til inderside af spærene
+        int distanceBetweenRafters = 60;
+
+        List<Integer> rafters = new ArrayList<>();
+
+        int totalLength = 0;
+
+        // Brug for-each-løkke til at tilføje spær og beregne længde der er tilbage
+        for (int i = 0; totalLength < lengthMM; i++) {
+            rafters.add(i);
+            totalLength += distanceBetweenRafters;
+        }
+
+        // Antallet af spær er størrelsen af listen
+        return rafters.size();
+
+    }
+
+}
