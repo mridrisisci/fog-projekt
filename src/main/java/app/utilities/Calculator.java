949f752a93e3c5a3a2d5aead80315e2ee371a3a1
Gervig <gervig91@gmail.com>
2024-11-29 10:30:47 +0100

    opdaterede calcRoofPlates logik

--- src/main/java/app/utilities/Calculator.java
+++ src/main/java/app/utilities/Calculator.java
@@ -9,7 +9,8 @@ public class Calculator
      * @param carportWidth  The width of the carport in cm.
      * @return An array where index 0 is the number of long plates, and index 1 is the number of short plates.
      */
-    public static int[] calcRoofPlates(int carportLength, int carportWidth) {
+    public static int[] calRoofPlates(int carportLength, int carportWidth)
+    {
         // Plate dimensions in cm
         final int LONG_PLATE_LENGTH = 600;
         final int SHORT_PLATE_LENGTH = 360;
@@ -20,23 +21,22 @@ public class Calculator
         int platesAlongWidth = (int) Math.ceil((double) carportWidth / PLATE_WIDTH);
 
         // If length is less than or equal to a single plate, no overlap needed
-        if (carportLength <= SHORT_PLATE_LENGTH) {
+        if (carportLength <= SHORT_PLATE_LENGTH)
+        {
             return new int[]{0, platesAlongWidth};
-        } else if (carportLength <= LONG_PLATE_LENGTH) {
+        } else if (carportLength <= LONG_PLATE_LENGTH)
+        {
             return new int[]{platesAlongWidth, 0};
         }
 
-        // Effective lengths considering overlap
-        int effectiveLongPlateLength = LONG_PLATE_LENGTH - OVERLAP;
-        int effectiveShortPlateLength = SHORT_PLATE_LENGTH - OVERLAP;
-
-        // Calculate how many long plates can fit
-        int longPlatesUsed = carportLength / effectiveLongPlateLength;
+        // Calculate how many long plates can fit with the overlap considered
+        // Calculate number of long plates needed to cover the length
+        int longPlatesUsed = 1 + (int) Math.floor((double) (carportLength - LONG_PLATE_LENGTH) / (LONG_PLATE_LENGTH - OVERLAP));
 
         // Calculate remaining length to cover
-        int remainingLength = carportLength - (longPlatesUsed * effectiveLongPlateLength);
+        int remainingLength = carportLength - (longPlatesUsed * LONG_PLATE_LENGTH - (longPlatesUsed - 1) * OVERLAP);
 
-        // Check if the remaining length can be covered by short plates
+        // Calculate how many short plates are needed for the remaining length
         int shortPlatesUsed = (remainingLength > 0) ?
                 (int) Math.ceil((double) remainingLength / SHORT_PLATE_LENGTH) : 0;
 
