4080bf58e7930254868e4914ec2238a25911c6d1
idrissalvatoreisci <158306705+idrissalvatoreisci@users.noreply.github.com>
2024-11-29 13:45:16 +0100

    ..

--- src/main/resources/public/sql/sql-tables.sql
+++ src/main/resources/public/sql/sql-tables.sql
@@ -11,14 +11,18 @@ DROP TABLE IF EXISTS public.postal_code CASCADE;
 DROP TABLE IF EXISTS public.materials CASCADE;
 DROP TABLE IF EXISTS public.orders_materials CASCADE;
 
+-- Old tables no longer used
+DROP TABLE IF EXISTS public.material_variants CASCADE;
+DROP TABLE IF EXISTS public.orders_material_variants CASCADE;
 
+-- Create tables
 CREATE TABLE IF NOT EXISTS public.accounts
 (
     account_id serial NOT NULL,
-    role character varying(11) COLLATE pg_catalog."default" NOT NULL,
-    username character varying(64) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(100) COLLATE pg_catalog."default",
-    password character varying(100) COLLATE pg_catalog."default",
+    role character varying(11) NOT NULL,
+    username character varying(64) NOT NULL,
+    email character varying(100),
+    password character varying(100),
     telephone integer,
     addresses_id integer NOT NULL,
     CONSTRAINT account_pk PRIMARY KEY (account_id)
@@ -27,7 +31,7 @@ CREATE TABLE IF NOT EXISTS public.accounts
 CREATE TABLE IF NOT EXISTS public.addresses
 (
     addresses_id serial NOT NULL,
-    address character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    address character varying(64) NOT NULL,
     postal_code_id integer NOT NULL,
     city_id integer NOT NULL,
     CONSTRAINT addresses_pkey PRIMARY KEY (addresses_id)
@@ -36,40 +40,40 @@ CREATE TABLE IF NOT EXISTS public.addresses
 CREATE TABLE IF NOT EXISTS public.cities
 (
     city_id serial NOT NULL,
-    city character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    city character varying(50) NOT NULL,
     CONSTRAINT cities_pkey PRIMARY KEY (city_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.materials
 (
     material_id serial NOT NULL,
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    unit character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(100) NOT NULL,
+    unit character varying(10) NOT NULL,
     price integer NOT NULL,
     length integer,
     height integer,
     width integer,
-    description character varying(100) COLLATE pg_catalog."default",
-    type character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    type character varying(50),
+    description character varying(100),
     CONSTRAINT material_pk PRIMARY KEY (material_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.orders
 (
     order_id serial NOT NULL,
-    carport_id character varying(8) COLLATE pg_catalog."default" NOT NULL,
+    carport_id character varying(8) NOT NULL,
     salesperson_id integer NOT NULL,
-    status character varying(20) COLLATE pg_catalog."default" NOT NULL,
+    status character varying(10) NOT NULL,
     price integer,
     sales_price integer,
     coverage_ratio_percentage integer,
-    order_placed timestamp with time zone NOT NULL,
+    order_placed timestamp with time zone,
     order_paid boolean NOT NULL,
     height integer NOT NULL,
     width integer NOT NULL,
-    length integer,
-    has_shed boolean NOT NULL,
-    roof_type character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    length integer NOT NULL,
+    "hasShed" boolean,
+    roof_type character varying(6) NOT NULL,
     account_id integer NOT NULL,
     CONSTRAINT orders_pk PRIMARY KEY (order_id)
 );
@@ -79,8 +83,20 @@ CREATE TABLE IF NOT EXISTS public.orders_materials
     orders_materials_id serial NOT NULL,
     order_id integer NOT NULL,
     material_id integer NOT NULL,
+    -- TODO tilføj NOT NULL til type, når vi har lavet logic for det
     quantity integer NOT NULL,
-    CONSTRAINT orders_materials_pkey PRIMARY KEY (orders_materials_id)
+    CONSTRAINT orders_materials_pk PRIMARY KEY (orders_materials_id),
+    --TODO tilføj denne CONSTRAIN når type logic er lavet.
+    -- Ideen er at et order_id, material_id og type forekommer unikt i tabellen
+    -- CONSTRAINT orders_materials_unique UNIQUE (order_id, material_id, type),
+    CONSTRAINT orders_materials_material_fk FOREIGN KEY (material_id)
+        REFERENCES public.materials (material_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT orders_materials_order_fk FOREIGN KEY (order_id)
+        REFERENCES public.orders (order_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS public.postal_code
@@ -90,47 +106,63 @@ CREATE TABLE IF NOT EXISTS public.postal_code
     CONSTRAINT postal_code_pkey PRIMARY KEY (postal_code_id)
 );
 
-ALTER TABLE IF EXISTS public.accounts
-    ADD CONSTRAINT accounts_addresses_fk FOREIGN KEY (addresses_id)
-        REFERENCES public.addresses (addresses_id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
-
-
-ALTER TABLE IF EXISTS public.addresses
+ALTER TABLE public.addresses
     ADD CONSTRAINT addresses_cities_fk FOREIGN KEY (city_id)
         REFERENCES public.cities (city_id) MATCH SIMPLE
         ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    ADD CONSTRAINT addresses_account_fk FOREIGN KEY (account_id)
+        REFERENCES public.accounts (account_id) MATCH SIMPLE
+        ON UPDATE CASCADE
         ON DELETE CASCADE;
 
-
-ALTER TABLE IF EXISTS public.addresses
+ALTER TABLE public.addresses
     ADD CONSTRAINT addresses_postal_code_fk FOREIGN KEY (postal_code_id)
         REFERENCES public.postal_code (postal_code_id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE;
 
-
-ALTER TABLE IF EXISTS public.orders
+ALTER TABLE public.orders
     ADD CONSTRAINT orders_account_fk FOREIGN KEY (account_id)
         REFERENCES public.accounts (account_id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE;
 
 
-ALTER TABLE IF EXISTS public.orders_materials
-    ADD CONSTRAINT material_id_fk FOREIGN KEY (material_id)
-        REFERENCES public.materials (material_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
+-- Insert data into materials
+INSERT INTO public.materials (name, unit, price, length, height, width, type, description)
+VALUES
+    ('25x200 mm. trykimp. Brædt', 'Stk', 177, 360, 25, 200, 'Understernbrædt', 'understernbrædder til for & bag ende'),
+    ('25x200 mm. trykimp. Brædt', 'Stk', 265, 540, 25, 200, 'Understernbrædt', 'understernbrædder til siderne'),
+    ('25x125mm. trykimp. Brædt', 'Stk', 119, 360, 25, 125, 'Oversternbrædt' , 'oversternbrædder til forenden'),
+    ('25x125mm. trykimp. Brædt', 'Stk', 178, 540, 25, 125, 'Oversternbrædt', 'oversternbrædder til siderne'),
+    ('38x73 mm. Lægte ubh.', 'Stk', 63, 420, 38, 73, 'Lægte', 'z på bagside af dør'),
+    ('45x95 mm. Reglar ub.', 'Stk', 35, 270, 45, 95, 'Reglar', 'løsholter til skur gavle'),
+    ('45x95 mm. Reglar ub.', 'Stk', 30, 240, 45, 95, 'Reglar', 'løsholter til skur sider'),
+    ('45x195 mm. spærtræ ubh.', 'Stk', 274, 480, 45, 195, 'Rem', 'Remme i sider, sadles ned i stolper (skur del, deles)'),
+    ('45x195 mm. spærtræ ubh.', 'Stk', 342, 600, 45, 195, 'Rem', 'Remme i sider, sadles ned i stolper (skur del, deles)'),
+    ('45x195 mm. spærtræ ubh.', 'Stk', 274, 480, 45, 195, 'Spær', 'Spær, monteres på rem'),
+    ('45x195 mm. spærtræ ubh.', 'Stk', 342, 600, 45, 195, 'Spær', 'Spær, monteres på rem'),
+    ('97x97 mm. trykimp. Stolpe', 'Stk', 266, 300, 97, 97, 'Stolpe', 'stolper nedgraves 90 cm i jord'),
+    ('19x100 mm. trykimp. Brædt', 'Stk', 19, 210, 19, 100, 'Beklædning', 'beklædning af skur 1 på 2'),
+    ('19x100 mm. trykimp. Brædt', 'Stk', 48, 540, 19, 100, 'Vandbrædt', 'vandbrædt på stern i sider'),
+    ('19x100 mm. trykimp. Brædt', 'Stk', 32, 360, 19, 100, 'Vandbrædt', 'vandbrædt på stern i forende'),
+    ('Plastmo Ecolite blåtonet', 'Stk', 339, 600, 2, 109, 'Tagplade', 'tagplader monteres på spær'),
+    ('Plastmo Ecolite blåtonet', 'Stk', 199, 360, 2, 109, 'Tagplade', 'tagplader monteres på spær'),
+    ('plastmo bundskruer 200 stk', 'Pakke', 429, 2, 1, 1, 'Bundskruer', 'skruer til tagplader'),
+    ('hulbånd 1x20 mm. 10 mtr.', 'Rulle', 349, 1000, 1, 20, 'Hulbånd', 'vindkryds på spær'),
+    ('universal 190 mm højre', 'Stk', 50, 5, 150, 5, 'Beslag - Højre', 'Til montering af spær på rem'),
+    ('universal 190 mm venstre', 'Stk', 50, 5, 150, 5, 'Beslag - Venstre', 'Til montering af spær på rem'),
+    ('4,5 x 60 mm. skruer 200 stk.', 'Pakke', 169, 6, 1, 1, 'Skruer', 'Til montering af stern & vandbrædt'),
+    ('4,0 x 50 mm. beslagskruer', 'Pakke', 139, 5, 1, 1, 'Beslagskruer', 'Til montering af universalbeslag + hulbånd'),
+    ('bræddebolt 10 x 120 mm.', 'Stk', 409, 1, 1, 1, 'Bræddebolt', 'Til montering af rem på stolper'),
+    ('firkantskiver 40x40x11mm', 'Stk', 9, 1, 1, 1, 'Firkantskiver','Til montering af rem på stolper'),
+    ('4,5 x 70 mm. Skruer 400 stk.', 'Pakke', 165, 7, 1, 1, 'Beklædningsskruer', 'Til montering af yderste beklædning'),
+    ('4,5 x 50 mm. Skruer 300 stk.', 'Pakke', 90, 5, 1, 1, 'Beklædningsskruer', 'Til montering af inderste beklædning'),
+    ('stalddørsgreb 50x75', 'Sæt', 269, 3, 1, 1, 'Stalddørsgreb', 'Til lås på dør i skur'),
+    ('t hængsel 390 mm', 'Stk', 139, 4, 1, 1, 'Hængsel', 'Til skurdør'),
+    ('vinkelbeslag 35', 'Stk', 1, 5, 5, 4, 'Vinkelbeslag', 'Til montering af løsholter i skur');
 
 
-ALTER TABLE IF EXISTS public.orders_materials
-    ADD CONSTRAINT orders_fk FOREIGN KEY (order_id)
-        REFERENCES public.orders (order_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID;
-
+-- End transaction
 END;
