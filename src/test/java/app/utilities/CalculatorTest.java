478abf1b2c05ffac70180cc54c063391c1c71776
Gervig <gervig91@gmail.com>
2024-12-03 09:22:14 +0100

    moved carports out to general test scope

--- src/test/java/app/utilities/CalculatorTest.java
+++ src/test/java/app/utilities/CalculatorTest.java
@@ -4,44 +4,45 @@ import app.entities.Carport;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class CalculatorTest
+{
+    Carport carport1 = new Carport(1, false, 360, 240);
+    Carport carport2 = new Carport(1, false, 600, 240);
+    Carport carport3 = new Carport(1, false, 780, 240);
+    Carport carport4 = new Carport(1, false, 360, 360);
+    Carport carport5 = new Carport(1, false, 600, 360);
+    Carport carport6 = new Carport(1, false, 0, 0);
 
     @Test
-    public void testCalcRoofPlates() {
-
+    public void testCalcRoofPlates()
+    {
         // Arrange and Act
         // Test case 1: Short plates only
-        Carport carport1 = new Carport(1, false, 360, 240);
         int[] actual1 = Calculator.calcRoofPlates(carport1);
         int expected1Long = 0;
         int expected1Short = 3;
 
         // Test case 2: Long plates only
-        Carport carport2 = new Carport(1, false, 600, 240);
         int[] actual2 = Calculator.calcRoofPlates(carport2);
         int expected2Long = 3;
         int expected2Short = 0;
 
         // Test case 3: Combination of long and short plates
-        Carport carport3 = new Carport(1, false, 780, 240);
         int[] actual3 = Calculator.calcRoofPlates(carport3);
         int expected3Long = 3;
         int expected3Short = 3;
 
         // Test case 4: Short plates only, with width adjustment
-        Carport carport4 = new Carport(1, false, 360, 360);
         int[] actual4 = Calculator.calcRoofPlates(carport4);
         int expected4Long = 0;
         int expected4Short = 4;
 
         // Test case 5: Long plates only, with width adjustment
-        Carport carport5 = new Carport(1, false, 600, 360);
         int[] actual5 = Calculator.calcRoofPlates(carport5);
         int expected5Long = 4;
         int expected5Short = 0;
 
         // Test case 6: Zero dimensions
-        Carport carport6 = new Carport(1, false, 0, 0);
         int[] actual6 = Calculator.calcRoofPlates(carport6);
         int expected6Long = 0;
         int expected6Short = 0;
