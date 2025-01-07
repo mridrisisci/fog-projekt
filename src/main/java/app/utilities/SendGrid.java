81c0fa41237d0bc248c31933ccc79b5174a0e667
Gervig <gervig91@gmail.com>
2025-01-07 11:39:25 +0100

    Update SendGrid.java

--- src/main/java/app/utilities/SendGrid.java
+++ src/main/java/app/utilities/SendGrid.java
@@ -59,7 +59,7 @@ public class SendGrid
 
         // Add category and template ID
         mail.addCategory("carportapp");
-        mail.setTemplateId("d-13710153c275487ca6ba870f31dc0d9f");
+        mail.setTemplateId("d-6ac3727740a24341b323aef7805519ae");
 
         com.sendgrid.SendGrid sg = new com.sendgrid.SendGrid(API_KEY);
         Request request = new Request();
@@ -215,7 +215,7 @@ public class SendGrid
 
         // Add category and template ID
         mail.addCategory("carportapp");
-        mail.setTemplateId("d-13710153c275487ca6ba870f31dc0d9f");
+        mail.setTemplateId("d-cd10b548b86849aea9d1785fce4952b7");
 
         com.sendgrid.SendGrid sg = new com.sendgrid.SendGrid(API_KEY);
         Request request = new Request();
